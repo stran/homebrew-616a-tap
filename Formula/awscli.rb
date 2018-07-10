72b11ed58a972f31ae504cdf5d0d6306994d81af
AJ Beck <aj.beck@healthstream.com>
2018-07-10 00:12:06 -0500

    add awscli v1.15.55

---
+++ Formula/awscli.rb
@@ -0,0 +1,53 @@
+class Awscli < Formula
+  include Language::Python::Virtualenv
+
+  desc "Official Amazon AWS command-line interface"
+  homepage "https://aws.amazon.com/cli/"
+  # awscli should only be updated every 10 releases on multiples of 10
+  url "https://github.com/aws/aws-cli/archive/1.15.55.tar.gz"
+  sha256 "d56da09f4f60593ad9333a88446854e4795fdf037a459f3c2907dbd1964a1f88"
+  head "https://github.com/aws/aws-cli.git", :branch => "develop"
+
+  bottle do
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "4ff55bc264d5f8517d6d02c5596ef2bbbe0ca3fe0595684c2f45e0bbdbf04046" => :high_sierra
+    sha256 "8c248007ffc883642780b9b28fd7408c4e50c176c3df35bd41b73c37246d3bd5" => :sierra
+    sha256 "a9b1ca959a8d226a404f40cd11386d5f92b0cace4a19aa524e5d64f6e072baa4" => :el_capitan
+  end
+
+  # Some AWS APIs require TLS1.2, which system Python doesn't have before High
+  # Sierra
+  depends_on "python"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
+                              "--ignore-installed", "PyYAML==3.13", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "awscli"
+    venv.pip_install_and_link buildpath
+    pkgshare.install "awscli/examples"
+
+    rm Dir["#{bin}/{aws.cmd,aws_bash_completer,aws_zsh_completer.sh}"]
+    bash_completion.install "bin/aws_bash_completer"
+    zsh_completion.install "bin/aws_zsh_completer.sh"
+    (zsh_completion/"_aws").write <<~EOS
+      #compdef aws
+      _aws () {
+        local e
+        e=$(dirname ${funcsourcetrace[1]%:*})/aws_zsh_completer.sh
+        if [[ -f $e ]]; then source $e; fi
+      }
+    EOS
+  end
+
+  def caveats; <<~EOS
+    The "examples" directory has been installed to:
+      #{HOMEBREW_PREFIX}/share/awscli/examples
+  EOS
+  end
+
+  test do
+    assert_match "topics", shell_output("#{bin}/aws help")
+  end
+end
