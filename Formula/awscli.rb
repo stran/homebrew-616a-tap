36fbf86adb2c43d8b461399a32d129cf77e7ec14
AJ Beck <aj.beck@healthstream.com>
2019-05-09 11:05:14 -0500

    update bottle sha

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -9,9 +9,9 @@ class Awscli < Formula
   head "https://github.com/aws/aws-cli.git", :branch => "develop"
 
   bottle do
-    root_url "https://github.com/ajb3ck/homebrew-616a-tap/releases/download/0.4.0"
+    root_url "https://github.com/ajb3ck/homebrew-616a-tap/releases/download/0.4.1"
     cellar :any_skip_relocation
-    sha256 "949a9bdd0c7e8b1de87a40d84cbe29d466da75d10457b4d9c53f8741048b8045" => :mojave
+    sha256 "14bd9a4624e570349dae002e583fce41e6c81066cc6cea9afffe663f4d1c71f9" => :mojave
   end
 
   # Some AWS APIs require TLS1.2, which system Python doesn't have before High
